ac2e4d16ed93f2a9733944803431ccbfe7046087
Jbmouterde <jeanbaptiste.mouterde@kedgebs.com>
2020-04-23 19:23:47 +0200

    done

--- script.js
+++ script.js
@@ -373,3 +373,19 @@ function draw() {
 }
 // start and run the game
 main();
+
+
+
+
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+//   if (!isChrome){
+//       $('#iframeAudio').remove()
+//   }
+//   else {
+//       $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+//   }
+
+
+//   window.onload = function() {
+// 	var context = new AudioContext();
+//   }
